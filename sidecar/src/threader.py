0c457b80968b6d5268c3bcbc1597da2972669712
noble-varghese <noblekvarghese96@gmail.com>
2024-03-29 18:09:50 +0530

    fix: minor fix

--- sidecar/src/threader.py
+++ sidecar/src/threader.py
@@ -31,7 +31,7 @@ class Worker:
 
     def poll_worker_queue(self):
         while True:
-            job_data = self.redis_conn.blpop(self.queue_name, timeout=4)
+            job_data = self.redis_conn.blpop(self.queue_name, timeout=1)
             if job_data:
                 job_data = job_data[1]  # Extracting the job data
                 print("Worker creation packet detected. Signaling to add a new worker.")
