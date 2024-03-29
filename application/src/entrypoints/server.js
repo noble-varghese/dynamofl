7c6f6c673a593b5771e80739227bfef19df3b4b7
noble-varghese <noblekvarghese96@gmail.com>
2024-03-29 18:28:45 +0530

    fix: minor fix

--- application/src/entrypoints/server.js
+++ application/src/entrypoints/server.js
@@ -18,11 +18,6 @@ export const startWebServer = async (cfgFile = null) => {
     app.all("/*", function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "X-Requested-With, orgkey, Content-Type, Authorization",
-        );
         next();
     });
     app.use(express.urlencoded({ extended: true }));
