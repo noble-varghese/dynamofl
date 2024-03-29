d8a5da46c65b72dce2be406fe7715b805ecffee2
noble-varghese <noblekvarghese96@gmail.com>
2024-03-29 19:30:37 +0530

    fix: minor fix

--- application/src/entrypoints/server.js
+++ application/src/entrypoints/server.js
@@ -14,10 +14,15 @@ export const startWebServer = async (cfgFile = null) => {
 
 
     // Helmet helps secure Express apps by setting HTTP response headers.
-    // app.use(helmet())
+    app.use(helmet())
     app.all("/*", function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "X-Requested-With, orgkey, Content-Type, Authorization",
+        );
         next();
     });
     app.use(express.urlencoded({ extended: true }));
