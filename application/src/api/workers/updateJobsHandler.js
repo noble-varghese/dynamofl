18701aa753d09b66a1b394f665a0a76ae7e8627c
noble-varghese <noblekvarghese96@gmail.com>
2024-03-29 14:16:15 +0530

    fix: minor fix

--- application/src/api/workers/updateJobsHandler.js
+++ application/src/api/workers/updateJobsHandler.js
@@ -16,7 +16,7 @@ export const updateJobsHandler = async (req, res, next) => {
             new ErrorHandlerClass(CLIENT_ERROR.statusCode, CLIENT_ERROR.message, { errors: errors.array() })
         )
     }
-    logger.info('Params are: ', req.params)
+    logger.info( req.params)
     const store = {
         jobId: req.params.tab_id,
         numFiles: req.body.num_files,
